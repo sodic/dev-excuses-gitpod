ee265d4ce4279aa52dd0d8d2ff166cc06c4dd4cd
Matija Sosic <matija.sosic@gmail.com>
2022-08-24 11:47:39 +0200

    added all the code.

--- MyNewApp/ext/MainPage.js
+++ MyNewApp/ext/MainPage.js
@@ -1,41 +1,56 @@
-import React from 'react'
-import waspLogo from './waspLogo.png'
-import './Main.css'
+import React, { useState } from 'react'
+import { useQuery } from '@wasp/queries'
+import getExcuse from '@wasp/queries/getExcuse'
+import getAllSavedExcuses from '@wasp/queries/getAllSavedExcuses'
+import saveExcuse from '@wasp/actions/saveExcuse'
 
 const MainPage = () => {
+  const [currentExcuse, setCurrentExcuse] = useState({ text: "" })
+  const { data: excuses } = useQuery(getAllSavedExcuses)
+
+  const handleGetExcuse = async () => {
+    try {
+      setCurrentExcuse(await getExcuse())
+    } catch (err) {
+      window.alert('Error while getting the excuse: ' + err.message)
+    }
+  }
+
+  const handleSaveExcuse = async () => {
+    if (currentExcuse.text) {
+      try {
+        await saveExcuse(currentExcuse)
+      } catch (err) {
+        window.alert('Error while saving the excuse: ' + err.message)
+      }
+    }
+  }
+
   return (
-    <div className="container">
-      <main>
-        <div className="logo">
-          <img src={waspLogo} alt="wasp" />
-        </div>
-
-        <h2 className="welcome-title"> Welcome to Wasp - you just started a new app! </h2>
-        <h3 className="welcome-subtitle">
-          This is page <code>MainPage</code> located at route <code>/</code>.
-          Open <code>ext/MainPage.js</code> to edit it.
-        </h3>
-
-        <div className="buttons">
-          <a
-            className="button button-filled"
-            href="https://wasp-lang.dev/docs/tutorials/todo-app"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Take the Tutorial
-          </a>
-          <a
-            className="button button-outline"
-            href="https://discord.com/invite/rzdnErX"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Chat on Discord
-          </a>
-        </div>
-      </main>
+    <div className="grid grid-cols-2 text-3xl">
+      <div>
+          <button onClick={handleGetExcuse} className="mx-2 my-1 p-2 bg-blue-600 hover:bg-blue-400 text-white rounded"> Get excuse </button>
+          <button onClick={handleSaveExcuse} className="mx-2 my-1 p-2 bg-blue-600 hover:bg-blue-400 text-white rounded"> Save excuse </button>
+        <Excuse excuse={currentExcuse} />
+      </div>
+      <div>
+        <div className="px-6 py-2 bg-blue-600 text-white"> Saved excuses: </div>
+        {excuses && <ExcuseList excuses={excuses} />}
+      </div>
     </div>
   )
 }
+
+const ExcuseList = (props) => { 
+  return props.excuses?.length ?  props.excuses.map((excuse, idx) => <Excuse excuse={excuse} key={idx} />) : 'No saved excuses'
+}
+
+const Excuse = ({ excuse }) => {
+  return (
+    <div className="px-6 py-2">
+      {excuse.text}
+    </div>
+  )
+}
+
 export default MainPage
